b21ef89956476cbb16de8d6e63ccb8293023784f
Anshul Jain <anshuljain21120@gmail.com>
2020-07-19 12:47:04 +0530

    compute part time employee's wage

--- ComputeEmployeeWage.java
+++ ComputeEmployeeWage.java
@@ -4,7 +4,8 @@ class ComputeEmployeeWage
 	public static void main(String args[])
 	{
 		System.out.println("Welcome to Employee Wage Computation Program!");
-		// Constants Definitions	
+		// Constants Definitions
+		int IS_PART_TIME = 1;
 		int IS_FULL_TIME = 1;
 		int EMP_RATE_PER_HOUR = 20;
 		// Variable Definitions
@@ -12,7 +13,9 @@ class ComputeEmployeeWage
 		int empWage = 0;
 			// Operation
 		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if(empCheck == IS_FULL_TIME)
+		if(empCheck == IS_PART_TIME)
+			empHrs = 4;
+		else if(empCheck == IS_FULL_TIME)
 			empHrs = 8;
 		else
 			empHrs = 0;
