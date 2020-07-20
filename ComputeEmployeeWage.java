126b1a765733e5592eca6b857999f37b2edb9d0b
Anshul Jain <anshuljain21120@gmail.com>
2020-07-21 02:04:03 +0530

    Refactord Code to fuse a class method

--- ComputeEmployeeWage.java
+++ ComputeEmployeeWage.java
@@ -4,39 +4,74 @@ class ComputeEmployeeWage
 	// Constants Definitions
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
-	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 20;
-	public static final int MAX_HOURS_IN_A_MONTH = 10;
 
-	public static void main(String args[])
+	private int calculateWage(	int employeeID,
+								int partTimeHrs, 
+								int fullTimeHrs, 
+								int wagePerHour, 
+								int daysWorked, 
+								int hoursWorked	)
 	{
+		// Function description & welcome message
 		System.out.println("Welcome to Employee Wage Computation Program!");
+
 		// Variable Definitions
 		int empHrs = 0;
 		int totalEmpHrs = 0;
 		int totalEmpWage = 0;
 		int totalWorkingDays = 0;
-			// Operation
-		while(totalEmpHrs <= MAX_HOURS_IN_A_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) 
+
+		// Operation
+		while(totalEmpHrs <= hoursWorked && totalWorkingDays < daysWorked) 
 		{
 			totalWorkingDays++;
 			double empCheck = Math.floor(Math.random() * 10) % 3;
 			switch ((int)empCheck)
 			{
 				case IS_PART_TIME:
-					empHrs = 4;
+					empHrs = partTimeHrs;
 					break;
 				case IS_FULL_TIME:
-					empHrs = 8;
+					empHrs = fullTimeHrs;
 					break;
 				default:
 					empHrs = 0;
 					break;
 			}
 			totalEmpHrs += empHrs;
-			System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " + empHrs);
+			System.out.println("\tDay#: "+totalWorkingDays+"\tHours woked today: "+empHrs+"\tToday's Wage: "+empHrs*wagePerHour);
 		}
-		totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Total Emp Wage: " + totalEmpWage);
+		totalEmpWage = totalEmpHrs * wagePerHour;
+		System.out.println("Employee with eID#"+employeeID+" got total monthly wage of "+totalEmpWage+" Rs by working "+totalWorkingDays+" day(s) totalling "+totalEmpHrs+" hour(s).");
+		
+		return totalEmpWage;
+	}
+
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter employee ID: ");
+		int employeeID=sc.nextInt();	
+
+		System.out.print("Enter part time hours: ");
+		int partTimeHrs=sc.nextInt();
+		
+		System.out.print("Enter full time hours: ");
+		int fullTimeHrs=sc.nextInt();
+		
+		System.out.print("Enter wage in Rs given per Hour : ");
+		int wagePerHour=sc.nextInt();
+		
+		System.out.print("Enter number of days employee worked: ");
+		int daysWorked=sc.nextInt();
+		
+		System.out.print("Enter number of hours employee worked: ");
+		int hoursWorked=sc.nextInt();
+		
+		ComputeEmployeeWage Alice = new ComputeEmployeeWage();
+		Alice.calculateWage(employeeID, partTimeHrs, fullTimeHrs, wagePerHour, daysWorked, hoursWorked);
+		
+		sc.close();
 	}
 }
